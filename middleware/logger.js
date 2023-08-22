578002ab830e2db3a7c2768973c7f124d69b3f58
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-08-22 07:18:36 +0600

    clean up: comments

--- middleware/logger.js
+++ middleware/logger.js
@@ -38,19 +38,3 @@ const loggerMiddleware = (req, res, next) => {
 }
 
 module.exports = { logEvents, loggerMiddleware }
-
-// const app = express()
-// const winston = require('winston')
-// const consoleTransport = new winston.transports.Console()
-// const myWinstonOptions = { transports: [consoleTransport] }
-// const logger = new winston.createLogger(myWinstonOptions)
-// function logRequest(req, res, next) {
-//   logger.info(req.url)
-//   next()
-// }
-// app.use(logRequest)
-// function logError(err, req, res, next) {
-//   logger.error(err)
-//   next()
-// }
-// app.use(logError)
