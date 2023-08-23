cc85ad116412a4d7a3185cb2076299a1bce9150a
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-08-23 15:45:01 +0600

    add: create new user controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt')
 
 const { BadRequestError, NotFoundError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
+const { response } = require('express')
 
 // @desc Get All Users
 // @route GET /users
@@ -21,7 +22,48 @@ const getAllUsers = asyncHandler(async (req, res) => {
 // @desc Create a New User
 // @route POST /users
 // @access Private
-const createNewUser = asyncHandler(async (req, res) => {})
+const createNewUser = asyncHandler(async (req, res) => {
+  console.log('hi')
+  const { username, password, roles } = req.body
+
+  // confirm data
+
+  if (!username || !password || !Array.isArray(roles) || !roles.length) {
+    throw new BadRequestError('All fields are required.')
+
+    // TODO: check if error thrown
+    // return res.status(400).json({message: 'All fields are required.'})
+  }
+
+  // check for duplicate , lean for faster lookup
+  const duplicate = await User.findOne({ username }).lean().exec()
+
+  if (duplicate) {
+    return res
+      .status(StatusCodes.CONFLICT)
+      .json({ message: 'Duplicate username' })
+  }
+
+  // Hash password
+  // TODO: trasfer to model
+  //   const hashedPwd = await bcrypt.hash(password, 10)
+
+  //   const userObject = { username, password: hashedPwd, roles }
+
+  // Create and store new user
+  // middleware hashes the password
+  const user = await User.create({ ...req.body })
+  //   console.log(user)
+
+  // successfully created user
+  if (user) {
+    res
+      .status(StatusCodes.CREATED)
+      .json({ message: `New user ${username} created` })
+  } else {
+    throw new BadRequestError('Invalid User Data Received')
+  }
+})
 
 // @desc Update a User
 // @route PATCH /users
