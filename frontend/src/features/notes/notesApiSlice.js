d6a7bac83e3578c550685064fe16c2cb46a20478
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-09-04 07:14:36 +0600

    add: mutations

--- frontend/src/features/notes/notesApiSlice.js
+++ frontend/src/features/notes/notesApiSlice.js
@@ -15,7 +15,6 @@ export const notesApiSlice = apiSlice.injectEndpoints({
       validateStatus: (response, result) => {
         return response.status === 200 && !result.isError
       },
-      keepUnusedDataFor: 5,
       transformResponse: (responseData) => {
         const loadedNotes = responseData.map((note) => {
           note.id = note._id
@@ -32,10 +31,43 @@ export const notesApiSlice = apiSlice.injectEndpoints({
         } else return [{ type: 'Note', id: 'LIST' }]
       },
     }),
+    addNewNote: builder.mutation({
+      query: (initialNote) => ({
+        url: '/notes',
+        method: 'POST',
+        body: {
+          ...initialNote,
+        },
+      }),
+      invalidatesTags: [{ type: 'Note', id: 'LIST' }],
+    }),
+    updateNote: builder.mutation({
+      query: (initialNote) => ({
+        url: '/notes',
+        method: 'PATCH',
+        body: {
+          ...initialNote,
+        },
+      }),
+      invalidatesTags: (result, error, arg) => [{ type: 'Note', id: arg.id }],
+    }),
+    deleteNote: builder.mutation({
+      query: ({ id }) => ({
+        url: `/notes`,
+        method: 'DELETE',
+        body: { id },
+      }),
+      invalidatesTags: (result, error, arg) => [{ type: 'Note', id: arg.id }],
+    }),
   }),
 })
 
-export const { useGetNotesQuery } = notesApiSlice
+export const {
+  useGetNotesQuery,
+  useAddNewNoteMutation,
+  useUpdateNoteMutation,
+  useDeleteNoteMutation,
+} = notesApiSlice
 
 // returns the query result object
 export const selectNotesResult = notesApiSlice.endpoints.getNotes.select()
