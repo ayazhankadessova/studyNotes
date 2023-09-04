8f91f829097c375dd16548d0cafdd6fcf6517c03
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-09-04 12:20:46 +0600

    add: editUser

--- frontend/src/features/users/EditUser.js
+++ frontend/src/features/users/EditUser.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { selectUserById } from './usersApiSlice'
+import EditUserForm from './EditUserForm'
+
+// get user id out of the url
+// get user data from the state, not query
+// we created useSelector in the userApiSlice
+// if we have a user, pull the edit user form that we have not created yet
 
 const EditUser = () => {
-  return <div></div>
+  const { id } = useParams()
+
+  const user = useSelector((state) => selectUserById(state, id))
+
+  const content = user ? <EditUserForm user={user} /> : <p>Loading...</p>
+
+  return content
 }
 
 export default EditUser
