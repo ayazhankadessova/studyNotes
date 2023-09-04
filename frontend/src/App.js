43611010a41a46572a2dc0e1faa008bb0ffdfe05
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-09-04 13:50:39 +0600

    add: prefetch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import EditUser from './features/users/EditUser'
 import NewUserForm from './features/users/NewUserForm'
 import EditNote from './features/notes/EditNote'
 import NewNote from './features/notes/NewNote'
+import Prefetch from './features/auth/Prefetch'
 
 function App() {
   return (
@@ -19,24 +20,26 @@ function App() {
         <Route index element={<Public />} />
         <Route path='login' element={<Login />} />
         {/* protected layouts */}
-        {/* nested routes  -> wrap around other components that are protected inside this route*/}
-        <Route path='dash' element={<DashLayout />}>
-          <Route index element={<Welcome />} />
+        <Route element={<Prefetch />}>
+          {/* nested routes  -> wrap around other components that are protected inside this route*/}
+          <Route path='dash' element={<DashLayout />}>
+            <Route index element={<Welcome />} />
 
-          <Route path='users'>
-            <Route index element={<UsersList />} />
-            <Route path=':id' element={<EditUser />} />
-            <Route path='new' element={<NewUserForm />} />
-          </Route>
+            <Route path='users'>
+              <Route index element={<UsersList />} />
+              <Route path=':id' element={<EditUser />} />
+              <Route path='new' element={<NewUserForm />} />
+            </Route>
 
-          {/* will add new note component, create note compinent, etc */}
-          <Route path='notes'>
-            <Route index element={<NotesList />} />
-            <Route path=':id' element={<EditNote />} />
-            <Route path='new' element={<NewNote />} />
-          </Route>
-        </Route>{' '}
-        {/*End Dash*/}
+            {/* will add new note component, create note compinent, etc */}
+            <Route path='notes'>
+              <Route index element={<NotesList />} />
+              <Route path=':id' element={<EditNote />} />
+              <Route path='new' element={<NewNote />} />
+            </Route>
+          </Route>{' '}
+          {/*End Dash*/}
+        </Route>
       </Route>
     </Routes>
   )
