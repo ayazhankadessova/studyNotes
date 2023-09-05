a13f0a43b14bf2813f26ef01ce8e06428cb672e5
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-09-05 22:00:52 +0800

    fix: content

--- frontend/src/features/notes/EditNote.js
+++ frontend/src/features/notes/EditNote.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { selectNoteById } from './notesApiSlice'
+import { selectAllUsers } from '../users/usersApiSlice'
+import EditNoteForm from './EditNoteForm'
+
+// bring data & edit pre-populated form to render that data
+// get note with that id
+// get users
+// content -> check if we have users & note -> pre-populate EditNoteForm with note & users
 
 const EditNote = () => {
-  return <div></div>
+  const { id } = useParams()
+
+  const note = useSelector((state) => selectNoteById(state, id))
+  const users = useSelector(selectAllUsers)
+
+  const content =
+    note && users ? (
+      <EditNoteForm note={note} users={users} />
+    ) : (
+      <p>Loading...</p>
+    )
+
+  return content
 }
 
 export default EditNote
