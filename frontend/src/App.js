c4e73d1dfc4bea99b7b314f3895e0fc50b9295e2
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-09-05 22:52:51 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,7 +35,7 @@ function App() {
             <Route path='notes'>
               <Route index element={<NotesList />} />
               <Route path=':id' element={<EditNote />} />
-              <Route path='new' element={<NewNote />} />
+              <Route path='new' element={<NewNote />} />{' '}
             </Route>
           </Route>{' '}
           {/*End Dash*/}
