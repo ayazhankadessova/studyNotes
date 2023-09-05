1fa83bce887d6cd59634ddf160abf9b7f46dba9a
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-09-05 22:08:07 +0800

    add: content & onchange

--- frontend/src/features/notes/EditNoteForm.js
+++ frontend/src/features/notes/EditNoteForm.js
@@ -37,29 +37,56 @@ const EditNoteForm = ({ note, users }) => {
   // handlers
   const onTitleChanged = (e) => setTitle(e.target.value)
   const onTextChanged = (e) => setText(e.target.value)
+  const onCompletedChanged = (e) => setCompleted(e.target.value)
+  const onUserIdChanged = (e) => setUserId(e.target.value)
 
   // check if we can save
   // All of these methods should be true
   // if not loading -> can save is true
 
-  let canSave
+  const canSave = [title, text, userId].every(Boolean) && !isLoading
 
-  canSave = [roles.length, validUsername].every(Boolean) && !isLoading
+  // on save note clicked -> update note
+
+  const onSaveNoteClicked = async (e) => {
+    if (canSave) {
+      await updateNote({ id: note.id, user: userId, title, text, completed })
+    }
+  }
+
+  const onDeleteNoteClicked = async () => {
+    await deleteNote({ id: note.id })
+  }
+  const created = new Date(note.createdAt).toLocaleString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  })
+  const updated = new Date(note.updatedAt).toLocaleString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  })
+
+  const options = users.map((user) => {
+    return (
+      <option key={user.id} value={user.id}>
+        {' '}
+        {user.username}
+      </option>
+    )
+  })
 
   const errClass = isError || isDelError ? 'errmsg' : 'offscreen'
-  const validUserClass = !validUsername ? 'form__input--incomplete' : ''
-  const validPwdClass =
-    password && !validPassword ? 'form__input--incomplete' : ''
-  const validRolesClass = !Boolean(roles.length)
-    ? 'form__input--incomplete'
-    : ''
-
-  // check for update / del error
+  const validTitleClass = !title ? 'form__input--incomplete' : ''
+  const validTextClass = !text ? 'form__input--incomplete' : ''
   const errContent = (error?.data?.message || delerror?.data?.message) ?? ''
-
-  // has save & delete buttons -> they call mutations
-  // add active/not buttons
-  // other fields are same
 
   const content = (
     <>
@@ -67,12 +94,12 @@ const EditNoteForm = ({ note, users }) => {
 
       <form className='form' onSubmit={(e) => e.preventDefault()}>
         <div className='form__title-row'>
-          <h2>Edit User</h2>
+          <h2>Edit Note #{note.ticket}</h2>
           <div className='form__action-buttons'>
             <button
               className='icon-button'
               title='Save'
-              onClick={onSaveUserClicked}
+              onClick={onSaveNoteClicked}
               disabled={!canSave}
             >
               <FontAwesomeIcon icon={faSave} />
@@ -80,71 +107,86 @@ const EditNoteForm = ({ note, users }) => {
             <button
               className='icon-button'
               title='Delete'
-              onClick={onDeleteUserClicked}
+              onClick={onDeleteNoteClicked}
             >
               <FontAwesomeIcon icon={faTrashCan} />
             </button>
           </div>
         </div>
-        <label className='form__label' htmlFor='username'>
-          Username: <span className='nowrap'>[3-20 letters]</span>
+        <label className='form__label' htmlFor='note-title'>
+          Title:
         </label>
         <input
-          className={`form__input ${validUserClass}`}
-          id='username'
-          name='username'
+          className={`form__input ${validTitleClass}`}
+          id='note-title'
+          name='title'
           type='text'
           autoComplete='off'
-          value={username}
-          onChange={onUsernameChanged}
+          value={title}
+          onChange={onTitleChanged}
         />
 
-        <label className='form__label' htmlFor='password'>
-          Password: <span className='nowrap'>[empty = no change]</span>{' '}
-          <span className='nowrap'>[4-12 chars incl. !@#$%]</span>
+        <label className='form__label' htmlFor='note-text'>
+          Text:
         </label>
-        <input
-          className={`form__input ${validPwdClass}`}
-          id='password'
-          name='password'
-          type='password'
-          value={password}
-          onChange={onPasswordChanged}
+        <textarea
+          className={`form__input form__input--text ${validTextClass}`}
+          id='note-text'
+          name='text'
+          value={text}
+          onChange={onTextChanged}
         />
+        <div className='form__row'>
+          <div className='form__divider'>
+            <label
+              className='form__label form__checkbox-container'
+              htmlFor='note-completed'
+            >
+              WORK COMPLETE:
+              <input
+                className='form__checkbox'
+                id='note-completed'
+                name='completed'
+                type='checkbox'
+                checked={completed}
+                onChange={onCompletedChanged}
+              />
+            </label>
 
-        <label
-          className='form__label form__checkbox-container'
-          htmlFor='user-active'
-        >
-          ACTIVE:
-          <input
-            className='form__checkbox'
-            id='user-active'
-            name='user-active'
-            type='checkbox'
-            checked={active}
-            onChange={onActiveChanged}
-          />
-        </label>
-
-        <label className='form__label' htmlFor='roles'>
-          ASSIGNED ROLES:
-        </label>
-        <select
-          id='roles'
-          name='roles'
-          className={`form__select ${validRolesClass}`}
-          multiple={true}
-          size='3'
-          value={roles}
-          onChange={onRolesChanged}
-        >
-          {options}
-        </select>
+            <label
+              className='form__label form__checkbox-container'
+              htmlFor='note-username'
+            >
+              ASSIGNED TO:
+            </label>
+            <select
+              id='note-username'
+              name='username'
+              className='form__select'
+              value={userId}
+              onChange={onUserIdChanged}
+            >
+              {options}
+            </select>
+          </div>
+          <div className='form__divider'>
+            <p className='form__created'>
+              Created:
+              <br />
+              {created}
+            </p>
+            <p className='form__updated'>
+              Updated:
+              <br />
+              {updated}
+            </p>
+          </div>
+        </div>
       </form>
     </>
   )
 
   return content
 }
+
 export default EditNoteForm
