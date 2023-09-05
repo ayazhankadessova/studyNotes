f8d2d99ea895ad505fca1d8ea95b738f8e82872d
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-09-05 22:01:30 +0800

    add: update interval

--- frontend/src/features/notes/NotesList.js
+++ frontend/src/features/notes/NotesList.js
@@ -11,7 +11,11 @@ const NotesList = () => {
     isSuccess,
     isError,
     error,
-  } = useGetNotesQuery()
+  } = useGetNotesQuery(undefined, {
+    pollingInterval: 15000,
+    refetchOnFocus: true,
+    refetchOnMountOrArgChange: true,
+  })
 
   let content
 
