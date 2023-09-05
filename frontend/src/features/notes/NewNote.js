f7574117d12a3bd64d974e38aca90008ef39b3a8
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-09-05 22:01:07 +0800

    Update NewNote.js

--- frontend/src/features/notes/NewNote.js
+++ frontend/src/features/notes/NewNote.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { selectAllUsers } from '../users/usersApiSlice'
+import NewNoteForm from './NewNoteForm'
+
+// get users
+// attach newnoteform for users
 
 const NewNote = () => {
-  return <div></div>
+  const users = useSelector(selectAllUsers)
+
+  const content = users ? <NewNoteForm users={users} /> : <p>Loading...</p>
+
+  return content
 }
 
 export default NewNote
