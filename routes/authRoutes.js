69a9fe5dcb646383d8dd2eb3ed5124266f327658
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-09-08 12:45:48 +0800

    add: route controllers

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,10 +2,12 @@ const express = require('express')
 const router = express.Router()
 const limiter = require('../controllers/authController')
 
-router.route('/').post(limiter)
+const { login, refresh, logout } = require('../controllers/authController')
 
-router.route('/refresh').get()
+router.route('/').post(limiter, login)
 
-router.route('/logout').post()
+router.route('/refresh').get(refresh)
+
+router.route('/logout').post(logout)
 
 module.exports = router
