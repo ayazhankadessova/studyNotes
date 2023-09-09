ea47b86321e99884b468976e5735c4623e8f924a
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-09-10 00:06:30 +0800

    Update root.js

--- routes/root.js
+++ routes/root.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 
-
 // look into the views folder & look at the index.html file
 router.get('^/$|/index(.html)?', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
