90e3fe98130a42d8e3663d3b9b4dc2c429074f82
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-09-10 00:06:54 +0800

    add: verify JWT

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,9 @@ const {
   updateUser,
   deleteUser,
 } = require('../controllers/usersController')
+const verifyJWT = require('../middleware/verifyJWT')
+// now verifyJWT is applied to all of the routes inside the router
+router.use(verifyJWT)
 
 // look into the views folder & look at the index.html file
 router
