9bb3147590d3c804550a93a4619d5c910e73f51d
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-09-11 07:19:13 +0800

    add: login

--- frontend/src/components/DashHeader.js
+++ frontend/src/components/DashHeader.js
@@ -1,13 +1,59 @@
-import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate, Link, useLocation } from 'react-router-dom'
+
+import { useSendLogoutMutation } from '../features/auth/authApiSlice'
+
+const DASH_REGEX = /^\/dash(\/)?$/
+const NOTES_REGEX = /^\/dash\/notes(\/)?$/
+const USERS_REGEX = /^\/dash\/users(\/)?$/
+
+// destructure pathname from the useLocation hook
+// sendLogout from Logout mutation hook
+// add more buttons later when we have diff permissions worked out
 
 const DashHeader = () => {
+  const navigate = useNavigate()
+  const { pathname } = useLocation()
+
+  const [sendLogout, { isLoading, isSuccess, isError, error }] =
+    useSendLogoutMutation()
+
+  useEffect(() => {
+    if (isSuccess) navigate('/')
+  }, [isSuccess, navigate])
+
+  if (isLoading) return <p>Logging Out...</p>
+
+  if (isError) return <p>Error: {error.data?.message}</p>
+
+  let dashClass = null
+  // make sure we are not on the notes list, users list or the dash itself
+  if (
+    !DASH_REGEX.test(pathname) &&
+    !NOTES_REGEX.test(pathname) &&
+    !USERS_REGEX.test(pathname)
+  ) {
+    dashClass = 'dash-header__container--small'
+  }
+
+  const logoutButton = (
+    <button className='icon-button' title='Logout' onClick={sendLogout}>
+      <FontAwesomeIcon icon={faRightFromBracket} />
+    </button>
+  )
+
   const content = (
     <header className='dash-header'>
-      <div className='dash-header__container'>
+      <div className={`dash-header__container ${dashClass}`}>
         <Link to='/dash'>
-          <h1 className='dash-header__title'>StudyNotes</h1>
+          <h1 className='dash-header__title'>techNotes</h1>
         </Link>
-        <nav className='dash-header__nav'>{/* add nav buttons later */}</nav>
+        <nav className='dash-header__nav'>
+          {/* add more buttons later */}
+          {logoutButton}
+        </nav>
       </div>
     </header>
   )
