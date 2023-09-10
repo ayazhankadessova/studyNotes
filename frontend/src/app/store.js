2af004e33c4f496223e02ecb7ddb6821f7819d5b
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-09-10 15:22:39 +0800

    add: import auth reducer

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -2,6 +2,8 @@ import { configureStore } from '@reduxjs/toolkit'
 import { apiSlice } from './api/apiSlice'
 import { setupListeners } from '@reduxjs/toolkit/query'
 
+import authReducer from '../features/auth/authSlice'
+
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
