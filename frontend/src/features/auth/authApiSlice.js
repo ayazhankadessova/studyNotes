7ead78804d7c40acff93c98636449416fccd77b2
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-09-10 15:22:43 +0800

    Create authApiSlice.js

---
+++ frontend/src/features/auth/authApiSlice.js
@@ -0,0 +1,11 @@
+import { apiSlice } from '../../app/api/apiSlice'
+// log out Action Creator
+import { logOut } from './authSlice'
+
+export const authApiSlice = apiSlice.injectEndpoints({
+    // onject inside of the builder where we can put each endpoint
+    // auth post
+    // auth logout 
+    // auth refresh
+    endpoints: builder = 
+})
