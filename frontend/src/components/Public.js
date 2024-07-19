c84ea65ad3917c7d8a055e38bf7d8dc5c0fda76e
ayazhankadessova <kadessovaayazhan@gmail.com>
2024-07-19 22:21:48 +0800

    fix: public page

--- frontend/src/components/Public.js
+++ frontend/src/components/Public.js
@@ -1,8 +1,9 @@
+import React from 'react'
 import { Link } from 'react-router-dom'
 import { Typography, Button, Box } from '@mui/material'
 
 const Public = () => {
-  const content = (
+  return (
     <section className='public'>
       <Box
         display='flex'
@@ -10,30 +11,27 @@ const Public = () => {
         alignItems='center'
         justifyContent='center'
         height='100vh'
+        textAlign='center'
+        sx={{ px: 2, py: 5 }}
       >
-        <header>
-          <Typography variant='h1' component='h1' gutterBottom>
-            Welcome to <span className='nowrap'>Study Notes!</span>
-          </Typography>
-        </header>
-        <main className='public__main'>
-          <Typography variant='body1' gutterBottom>
-            Collaborate on group projects & Store your notes here!
-          </Typography>
-          <Button
-            component={Link}
-            to='/login'
-            variant='contained'
-            color='primary'
-            style={{ marginTop: '30px' }}
-          >
-            Login
-          </Button>
-        </main>
+        <Typography variant='h1' component='h1' gutterBottom>
+          Welcome to <span className='nowrap'>Study Notes!</span>
+        </Typography>
+        <Typography variant='body1' gutterBottom>
+          Collaborate on group projects & store your notes here!
+        </Typography>
+        <Button
+          component={Link}
+          to='/login'
+          variant='contained'
+          color='primary'
+          sx={{ mt: 4, px: 3, py: 3 }} // Adjust padding for better button size
+        >
+          Get Started
+        </Button>
       </Box>
     </section>
   )
-  return content
 }
 
 export default Public
