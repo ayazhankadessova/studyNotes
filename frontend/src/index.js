bf784f08ea6c1a73ef80f9f7b92a63e0de31f278
ayazhankadessova <kadessovaayazhan@gmail.com>
2024-07-19 18:22:50 +0800

    fix to react17

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,8 +7,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
