004a29aa778f455d0fa753a531d04aba1a415361
ayazhankadessova <kadessovaayazhan@gmail.com>
2024-07-22 21:45:19 +0800

    fix api

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   'http://localhost:3500',
   'https://study-notes-backend.vercel.app',
+  'https://study-notes-g5oshycrf-ayazhankadessovas-projects.vercel.app',
 ]
 // local hosts that we would accept to access our REST API
 
