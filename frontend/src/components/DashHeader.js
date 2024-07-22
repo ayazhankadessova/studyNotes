69aebaa35a37534556fce54bd9ffa53bd03dea36
ayazhankadessova <kadessovaayazhan@gmail.com>
2024-07-22 21:50:41 +0800

    fix: header

--- frontend/src/components/DashHeader.js
+++ frontend/src/components/DashHeader.js
@@ -48,7 +48,7 @@ const DashHeader = () => {
     <header className='dash-header'>
       <div className={`dash-header__container ${dashClass}`}>
         <Link to='/dash'>
-          <h1 className='dash-header__title'>techNotes</h1>
+          <h1 className='dash-header__title'>studyNotes</h1>
         </Link>
         <nav className='dash-header__nav'>{logoutButton}</nav>
       </div>
