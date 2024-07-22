1d8a298a0f1de85237af3daece234768bbf89cf8
ayazhankadessova <kadessovaayazhan@gmail.com>
2024-07-22 21:34:59 +0800

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,4 +1,7 @@
-const allowedOrigins = ['http://localhost:3500']
+const allowedOrigins = [
+  'http://localhost:3500',
+  'https://study-notes-backend.vercel.app',
+]
 // local hosts that we would accept to access our REST API
 
 module.exports = allowedOrigins
